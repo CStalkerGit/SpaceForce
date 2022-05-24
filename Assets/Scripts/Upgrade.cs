31d83855e9c0584343b6207d44b796291a0af505
CStalker <cstalker.coder@gmail.com>
2022-05-24 20:22:48 +0300

    revamped weapon system

---
+++ Assets/Scripts/Upgrade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Orientation
+{
+    Top,
+    Side,
+    Bottom,
+    Option
+}
+
+public class Upgrade : MonoBehaviour
+{
+    public Orientation orientation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static bool TestCompatibibly()
+    {
+        return false;
+    }
+}
