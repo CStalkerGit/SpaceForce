860fc8bb10aa3fd59cb1e5062b914932d96ab814
CStalker <cstalker.coder@gmail.com>
2022-07-09 02:03:42 +0300

    hud

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var v = transform.localPosition;
+        v.y -= Time.deltaTime * 2;
+        if (v.y < -4) v.y += 8;
+        transform.localPosition = v;
+    }
+}
