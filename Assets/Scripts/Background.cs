6515546dbd3071a9a2b357917217b31d2498ab9b
CStalker <cstalker.coder@gmail.com>
2022-07-19 12:57:10 +0300

    scrolling background

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
+    public float scrollSpeed;
+
     // Update is called once per frame
     void Update()
     {
         var v = transform.localPosition;
-        v.y -= Time.deltaTime * 2;
+        v.y -= Time.deltaTime * scrollSpeed;
         if (v.y < -4) v.y += 8;
         transform.localPosition = v;
     }
